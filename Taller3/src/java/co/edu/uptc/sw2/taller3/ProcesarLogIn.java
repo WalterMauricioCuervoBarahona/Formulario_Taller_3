61460bde9239fd842f0c6b46a4987933c9e0f656
walter_M_cuervo <walter.cuervo@uptc.edu.co>
2017-09-13 23:58:21 -0500

    taller 3
    se hace modificaciones al taller 3

---
+++ Taller3/src/java/co/edu/uptc/sw2/taller3/ProcesarLogIn.java
@@ -0,0 +1,36 @@
+package co.edu.uptc.sw2.taller3;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Walter Cuervo
+ */
+@WebServlet(name = "ProcesarLogIn", urlPatterns = {"/ProcesarlogIn"})
+public class ProcesarLogIn extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = resp.getWriter()){
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Login</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            String user = req.getParameter("admin");
+            String pass = req.getParameter("123");
+            out.println((user.compareTo(pass)==0?"Bienvenido usuario "+user:"Usuario o contraseña incorrecto"));
+            out.println("</body>");
+            out.println("</html>");
+        }catch (Exception e) {
+        }
+    }
+}
